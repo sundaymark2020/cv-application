f160dd708ae5d0f26ded412f8a2dbe6d26d85f72
Sunday Mark <166103886+sundaymark2020@users.noreply.github.com>
2025-07-21 18:23:36 +0100

    create the general section

--- src/components/generalinfo.jsx
+++ src/components/generalinfo.jsx
@@ -1,34 +1,56 @@
+import React from 'react';
 
-import { useState } from 'react'
-
-export default function GeneralInfo({data,onChange,isEditing}) {
-  function handleNameChange (e) {
-    onChange('name', e.target.value)
+export default function Generalinfo({ data, isEditing, onChange }) {
+  function changeName(e) {
+    onChange('name',e.target.value)
     
   }
 
-  function handleEmailChange(e) {
-    onChange('email',e.target.value)
-    
-  }
-
-  function handlePhoneChange(e) {
+  function changetel(e) {
     onchange('phone',e.target.value)
     
   }
-  return (
-      <div className='section'>
 
-      <h1>General Information</h1>
-     {isEditing?(
-      <>
-      <input type="text" placeholder='Name' value={data.name} onChange={handleNameChange}/>
-      <input type="email" placeholder='Email' value={data.email} onChange={handleEmailChange} />
-      <input type="tel"  placeholder='Phone Number' value={data.phone} onChange={handlePhoneChange}/>
-      </>
-     )}
-      
-      </div>
+  function changeemail(e) {
+    onChange('email', e.target.value)
     
-  )
+  }
+
+
+  return (
+    <div>
+      <h1>General Information</h1>
+      {isEditing ? (
+        <>
+          <input
+            type="text"
+            placeholder="Enter Name"
+            value={data.name}
+            onChange={changeName}
+            
+          />
+          <input
+            type="tel"
+            placeholder="Enter Phone Number"
+            value={data.phone}
+            onChange={changetel}
+            
+          />
+          <input
+            type="email"
+            placeholder="Enter Email"
+            value={data.email}
+            onChange={changeemail}
+          
+          />
+        </>
+      ) : (
+        <>
+          <h1>Name: {data.name}</h1>
+          <h1>Phone Number: {data.phone}</h1>
+          <h1>Email: {data.email}</h1>
+        </>
+      )}
+    </div>
+  );
 }
